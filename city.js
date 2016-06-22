8c199928ba38a078cba7ae92e9b26eb2092c4f7d
Luc Juggery <luc.juggery@gmail.com>
2016-06-22 21:20:33 +0200

    Provide port number as env variable

--- city.js
+++ city.js
@@ -3,7 +3,12 @@ var http   = require("http"),
     chance = require("chance"),
     util   = require("util");
 
+// Default port is 8080, overridden by PORT env variable if provided
+var port = process.env.PORT || 8080;
+
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ message: util.format("%s suggest to visit %s", ip.address(), chance().city())}));
-}).listen(8080);
+    response.end(JSON.stringify({ message: util.format("%s suggests to visit %s", ip.address(), chance().city())}));
+}).listen(port);
+
+console.log(util.format("waiting for request on port %s", port));
