ba2c9310ddbbae2eb05f645bad531c79e40f0fde
Luc Juggery <luc.juggery@gmail.com>
2016-06-23 22:33:17 +0200

    Add hostname in returned json

--- city.js
+++ city.js
@@ -1,14 +1,18 @@
-var http   = require("http"),
-    ip     = require("ip"),
-    chance = require("chance"),
-    util   = require("util");
+var http    = require("http"),
+    os      = require("os"),
+    ip      = require("ip"),
+    moment  = require("moment"),
+    chance  = require("chance"),
+    winston = require("winston"),    
+    util    = require("util");
 
 // Default port is 8080, overridden by PORT env variable if provided
 var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
+    winston.log("info", util.format("[%s] -> new request", moment().toISOString()));
     response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify({ message: util.format("%s suggests to visit %s", ip.address(), chance().city())}));
+    response.end(JSON.stringify({ message: util.format("%s [%s] suggests to visit %s", os.hostname(), ip.address(), chance().city())}));
 }).listen(port);
 
-console.log(util.format("waiting for request on port %s", port));
+winston.log("info", util.format("waiting for request on port %s", port));
